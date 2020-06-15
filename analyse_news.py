5abb60ddb508f5ec313193c0709654eeaeec919c
jakob-ra <jakob.rauch@student.uva.nl>
2020-06-15 12:40:39 +0200

    update

--- analyse_news.py
+++ analyse_news.py
@@ -1,3 +1,14 @@
 import pandas as pd
+from nltk.tag import StanfordNERTagger
+from nltk.tokenize import word_tokenize
+import tensorflow as tf
+import tensorflow_hub as hub
 
 df = pd.read_parquet('financial_data.parquet.gzip')
+
+# small data set to run fast experiments
+df_size = 1000
+df = df.head(df_size)
+
+df.iloc[500].Article
+
