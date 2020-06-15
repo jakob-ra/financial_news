e9998ae75d8641ab8701fc50224483ca176f7688
jakob-ra <jakob.rauch@student.uva.nl>
2020-06-15 15:45:55 +0200

    read Thomson data

--- analyse_news.py
+++ analyse_news.py
@@ -4,11 +4,28 @@ from nltk.tokenize import word_tokenize
 import tensorflow as tf
 import tensorflow_hub as hub
 
-df = pd.read_parquet('financial_data.parquet.gzip')
+# read Thomson SDC RND database
+relevant_colums = [6,13,26]
+column_names = ['date', 'text', 'participants'] # this extracts the PARENT COMPANY participants
+df = pd.read_excel('SDC_RND_2015_2020.xlsx', skiprows=1, usecols=relevant_colums, names=column_names)
+
+# keep only date not time
+df['date'] = df['date'].dt.date
+
+# separate multiline Participants cell into tuple
+df['participants'] = df['participants'].str.split('\n')
+
+# replace new line character \n with space in deal text
+df['text'] = df['text'].str.replace('\n', ' ')
+
+
+# participants = df.participants.str.split('\n', expand=True)
+# df.drop(columns='participants', inplace=True) # drop old participants column
+# df = df.join(participants)
+
+# read Reuters financial news database
+# df = pd.read_parquet('financial_data.parquet.gzip')
 
 # small data set to run fast experiments
-df_size = 1000
-df = df.head(df_size)
-
-df.iloc[500].Article
-
+# df_size = 1000
+# df = df.head(df_size)
