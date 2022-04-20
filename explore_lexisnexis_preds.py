0e8877bed0d872183af398c3c212f925c9375b2d
jakob-ra <jakob.rauch@student.uva.nl>
2022-04-20 14:03:44 +0200

    lexis match

--- explore_lexisnexis_preds.py
+++ explore_lexisnexis_preds.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
-import glob
-import numpy as np
 
 from firm_name_matching import firm_name_clean
 
@@ -74,54 +72,8 @@ plt.yticks(rotation=45, ha='right', size=6)
 plt.tight_layout()
 plt.show()
 
-
-
-
-## Match with ORBIS to replace firm names with BvD ID
-
-orbis_path = '/Users/Jakob/Documents/Orbis/lexis_firms_matched_orbis'
-all_orbis = glob.glob(os.path.join(orbis_path, "*.xlsx"))
-
-orbis = pd.concat((pd.read_excel(f) for f in all_orbis), ignore_index=True)
-
-orbis.drop(columns=['Unnamed: 0'], inplace=True)
-
-orbis.dropna(subset=['Company name Latin alphabet', 'BvD ID number'], inplace=True)
-
-# orbis['BvD ID number'] = orbis['BvD ID number'].ffill() # for aggregating the multi-line NACE codes
-# orbis['Company name Latin alphabet'] = orbis['Company name Latin alphabet'].ffill()
-
-# orbis.drop_duplicates(subset=['Company name Latin alphabet', 'BvD ID number'], inplace=True)
-
-def isnull(val):
-    nan_strings = ['', 'NaN', 'NA', 'na', 'n.a.', 'nan']
-    return pd.isnull(val) or val in nan_strings
-
-orbis = orbis.groupby(['Company name Latin alphabet', 'BvD ID number']).agg(list)
-orbis.reset_index(inplace=True)
-
-def first_not_null_in_list(vals: list):
-    for val in vals:
-        if not isnull(val):
-            return val
-    return np.nan
-
-for col in orbis.columns[2:]:
-    orbis[col] = orbis[col].apply(first_not_null_in_list)
-
-orbis['cleaned_name'] = orbis['Company name Latin alphabet'].apply(firm_name_clean)
-
-# sort by size (employees) first, then value added, then number of NA columns
-orbis['count_na'] = orbis.isnull().sum(1)
-orbis.sort_values(by=['Number of employees\n2020', 'Added value\nth USD 2020', 'count_na'],
-                          inplace=True, ascending=[False, False, True])
-
-# now keep only the biggest firms / firms with most complete records among those with the same name
-orbis.drop_duplicates(subset=['cleaned_name'], keep='first', inplace=True)
-
-orbis.to_pickle('C:/Users/Jakob/Documents/Orbis/orbis_michael_lexis_2.pkl')
+# match with orbis
 orbis = pd.read_pickle('C:/Users/Jakob/Documents/Orbis/orbis_michael_lexis_2.pkl')
-
 
 df['cleaned_firms'] = df.firms.apply(lambda firms: [firm_name_clean(firm) for firm in firms])
 
@@ -137,17 +89,23 @@ lexis_firm_names_clean['cleaned_name'] = lexis_firm_names_clean.cleaned_name.rep
 names_ids = lexis_firm_names_clean.merge(orbis[['cleaned_name', 'BvD ID number']],
                                          on='cleaned_name', how='left')
 
-# look at unmatched
-unmatched = names_ids[names_ids['BvD ID number'].isnull()].copy()
-
-orbis2 = pd.read_pickle('C:/Users/Jakob/Documents/Orbis/combined_firm_list.pkl')
-unmatched = unmatched[['cleaned_name']].merge(orbis2[['company', 'bvdidnumber']],
-                                         left_on='cleaned_name', right_on='company', how='left')
-unmatched.drop(columns=['cleaned_name'], inplace=True)
-unmatched.bvdidnumber.explode().to_csv(
-        'C:/Users/Jakob/Documents/Orbis/bvdids_michael_unmatched_3.csv', index=False)
-
 names_ids = names_ids.dropna().set_index('cleaned_name').squeeze().to_dict()
+
+df.cleaned_firms.explode().value_counts()
+
+
+
+
+# look at unmatched
+# unmatched = names_ids[names_ids['BvD ID number'].isnull()].copy()
+#
+# orbis2 = pd.read_pickle('C:/Users/Jakob/Documents/Orbis/combined_firm_list.pkl')
+# unmatched = unmatched[['cleaned_name']].merge(orbis2[['company', 'bvdidnumber']],
+#                                          left_on='cleaned_name', right_on='company', how='left')
+# unmatched.drop(columns=['cleaned_name'], inplace=True)
+# unmatched.bvdidnumber.explode().to_csv(
+#         'C:/Users/Jakob/Documents/Orbis/bvdids_michael_unmatched_3.csv', index=False)
+
 
 
 ## create edge list for Michael
---
+++ read_orbis_lexis.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import glob
+import os
+
+orbis_path = '/Users/Jakob/Documents/Orbis/lexis_firms_matched_orbis'
+all_orbis = glob.glob(os.path.join(orbis_path, "*.xlsx"))
+
+orbis = pd.concat((pd.read_excel(f) for f in all_orbis), ignore_index=True)
+
+orbis.drop(columns=['Unnamed: 0'], inplace=True)
+
+orbis.dropna(subset=['Company name Latin alphabet', 'BvD ID number'], inplace=True)
+
+# orbis['BvD ID number'] = orbis['BvD ID number'].ffill() # for aggregating the multi-line NACE codes
+# orbis['Company name Latin alphabet'] = orbis['Company name Latin alphabet'].ffill()
+
+# orbis.drop_duplicates(subset=['Company name Latin alphabet', 'BvD ID number'], inplace=True)
+
+def isnull(val):
+    nan_strings = ['', 'NaN', 'NA', 'na', 'n.a.', 'nan']
+    return pd.isnull(val) or val in nan_strings
+
+orbis = orbis.groupby(['Company name Latin alphabet', 'BvD ID number']).agg(list)
+orbis.reset_index(inplace=True)
+
+def first_not_null_in_list(vals: list):
+    for val in vals:
+        if not isnull(val):
+            return val
+    return np.nan
+
+for col in orbis.columns[2:]:
+    orbis[col] = orbis[col].apply(first_not_null_in_list)
+
+orbis['cleaned_name'] = orbis['Company name Latin alphabet'].apply(firm_name_clean)
+
+# sort by size (employees) first, then value added, then number of NA columns
+orbis['count_na'] = orbis.isnull().sum(1)
+orbis.sort_values(by=['Number of employees\n2020', 'Added value\nth USD 2020', 'count_na'],
+                          inplace=True, ascending=[False, False, True])
+
+# now keep only the biggest firms / firms with most complete records among those with the same name
+orbis.drop_duplicates(subset=['cleaned_name'], keep='first', inplace=True)
+
+orbis.to_pickle('C:/Users/Jakob/Documents/Orbis/orbis_michael_lexis_2.pkl')
